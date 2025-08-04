7304ef7c4e5742e7f6a33e1f48107c942b24f772
kaklilu45 <aklilu.kerubel@gmail.com>
2025-08-03 21:31:49 -0400

    Add files via upload
    -Created getMem function with description and docstrings
    -Working to make values output a whole number instead of a float

---
+++ memoryFunc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+
+'''
+The getMem function will parse through the data in /proc/meminfo by opening and
+displaying the first 3 lines of the file which contain general memory info into a dictionary
+'''
+
+def getMem():
+    meminfo = {}
+    f = open('/proc/meminfo', 'r')
+    count = 0
+    for line in f:
+        count = count + 1
+        if count > 3:
+            break
+        key = line.split(':')[0].strip() # Get description of numerical value
+        value = line.split(':')[1].strip().split()[0] #Get numerical value only
+        meminfo[key] = int(value)/1024  #convert kb to mb
+    f.close()
+    return meminfo
+
+if __name__ == "__main__":
+    print(getMem())
